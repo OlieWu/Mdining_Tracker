14b10b2ab8e8e3ac6836e620d0ee3bb7b24b5c23
OlieWu <oliverwu@umich.edu>
2023-12-17 20:54:31 -0500

    working changes

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
-from bs4 import BeautifulSoup
 import smtplib
+import os
+from bs4 import BeautifulSoup
 from email.message import EmailMessage
 from dotenv import load_dotenv
-import os
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 
 load_dotenv()
 
@@ -36,45 +39,34 @@ def send_email(msg):
             print(f"Failed to send email: {e}")
         finally:
             server.quit()
-
 found = False
 msg = ""
 for url in urls:
     try:
         print("Start")
         # Send a GET request to the URL
-        response = requests.get(url, verify=True)
+        response = requests.get(url, verify=False)
         print("Good response")
-
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
             # Get the HTML content
             html_content = response.text
-
             soup = BeautifulSoup(html_content, 'html.parser')
-
             # Find all divs with class="courses"
             courses_divs = soup.find_all('div', class_='courses')
-
             # Iterate through each courses div
             for courses_div in courses_divs:
                 # Find the preceding h3 tag
                 preceding_h3 = courses_div.find_previous_sibling('h3')
-
                 # Check if the preceding_h3 contains "Breakfast"
                 if preceding_h3 and 'Dinner' in preceding_h3.get_text():
-                    print("Inside Breakfast courses div")
                     # Find all ul tags with class="courses_wrapper" within courses_div
                     courses_wrapper_uls = courses_div.find_all('ul', class_='courses_wrapper')
-
                     # Get the last courses_wrapper_ul (Mbakery should be the last item)
                     last_courses_wrapper_ul = courses_wrapper_uls[-1] if courses_wrapper_uls else None
-
                     if last_courses_wrapper_ul:
-                        print("Inside last courses_wrapper_ul")
                         # Find all li items within the last courses_wrapper_ul
                         li_items = last_courses_wrapper_ul.find_all('li')
-
                         if li_items:
                             for li_item in li_items:
                                 if ('Chocolate Chunk Cookies' in li_item.get_text()):
@@ -84,15 +76,12 @@ for url in urls:
                                     break
                         else: 
                             print("no li_items")
-
         else:
             print(f"Failed to fetch the webpage. Status code: {response.status_code}")
-
     except requests.RequestException as e:
         print(f"Error fetching the webpage: {e}")
-
 # If no cookies were found in all dining halls
 if not found:
-    send_email("No Cookies Found", "No dining halls with cookies :(")
+    send_email("No dining halls with cookies :(")
 else:
     send_email(msg)
